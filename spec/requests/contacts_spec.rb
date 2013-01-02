267a885579bf8a44755020369d3217f2a14f5c95
EmanuelCadems <emanuel_cadems@hotmail.com>
2013-01-02 17:29:30 -0300

    completando spec

--- spec/requests/contacts_spec.rb
+++ spec/requests/contacts_spec.rb
@@ -3,19 +3,28 @@ describe "Manage contacts" do
   before :each do
     user = create(:user, email:'ma@gmail.com')
     sign_in user
+  end
 
-    Rails.logger.debug "*******************************************************"
-    Rails.logger.debug "user email: #{user.email} user password : #{user.password}"
-    Rails.logger.debug "*******************************************************"
+  it "shows a list of contacts" do
+    contact = create(:contact)
+    visit contacts_path
+    page.should have_content contact.firstname
+    page.should have_content contact.lastname
+    # page.should have_content contact.pho
+  end
+
+  it "shows a requested contact" do
+    contact = create(:contact)
+    visit contacts_path
+    click_link contact.name
+    
+    page.should have_content contact.name
   end
 
   it "deletes contact", js: true do
     contact = create(:contact, firstname: 'Juan', lastname: 'Alarcon')
-    Rails.logger.debug "*******************************************************"
-    Rails.logger.debug "Ultimo Contacto #{Contact.last.firstname}"
-    Rails.logger.debug "firstname: #{contact.firstname} lastname: #{contact.lastname} email:#{contact.email} phones: #{contact.phones}"
-    Rails.logger.debug "*******************************************************"
     visit root_path
+
     expect{
       within "#contact_#{contact.id}" do
         click_link "Destroy"
@@ -25,8 +34,36 @@ describe "Manage contacts" do
       alert.accept
       sleep(3)
     }.to change(Contact, :count).by(-1)
+
     page.should have_content "Contacts"
     page.should_not have_content "Juan Alarcon"
+  end
+
+  describe "New Contact" do
+    context "with a user registered" do
+      it "create a new contact" do
+        user = create(:user)
+        sign_in user
+        contact = create(:contact)
+
+        expect{
+          click_link 'New Contact'
+          fill_in 'Firstname', with: contact.firstname
+          fill_in 'Lastname', with: contact.lastname
+          fill_in 'Email', with: contact.email
+          fill_in 'home', with: contact.phones.where(:phone_type => 'home')
+          fill_in 'office', with: contact.phones.where(:phone_type => 'work')
+          fill_in 'mobile', with: contact.phones.where(:phone_type => 'mobile')
+          click_button 'Create Contact'
+          page.should have_content 'Contact was successfully created.'
+          page.should have_content contact.name
+        }.to change(Contact, :count).by(1)
+      end
+    end
+
+    context "without a user registered" do
+      pending
+    end
   end
 end
 
